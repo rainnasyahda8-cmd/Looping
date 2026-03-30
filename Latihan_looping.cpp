4a2db804e9b0e4d7e0889d027750561041483259
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-03-30 15:55:14 +0700

    menambahknkan cout

--- Latihan_looping.cpp
+++ Latihan_looping.cpp
@@ -9,7 +9,8 @@ int main(){
     //mencetak tulisan sebanyak 5 kali
     cout<<"PERULANGAN FOR"<<endl
     for(int i=0;i<=4;i++){
-        
+        cout<<"Teknologi Informasi UMY"<<endl;
     }
+    cout<<endl;
 
 }
